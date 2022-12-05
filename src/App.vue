5eaae42bf587ac92aa3511560841e9af80d3ac04
Diana Marcelin <marcelindiana@yahoo.com>
2022-12-04 19:43:28 -0500

    created form,icons clickable

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@
       <img src="./assets/pinia-logo.svg" alt="pinia logo" />
       <h1>Pinia Tasks</h1>
     </header>
+
+    <div class="new-task-form">
+      <TaskForm />
+    </div>
+
     <nav class="filter">
       <button @click="filter = 'all'">All Tasks</button>
       <button @click="filter = 'favs'">Fav Tasks</button>
@@ -11,13 +16,13 @@
 
     <div class="task-list" v-if="filter === 'all'">
       <p>You have {{ taskStore.totalCount }} tasks left to do</p>
-      <div v-for="task in taskStore.tasks">
+      <div v-for="task in taskStore.tasks" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
     <div class="task-list" v-if="filter === 'favs'">
       <p>You have {{ taskStore.favCount }} tasks left to do</p>
-      <div v-for="task in taskStore.favs">
+      <div v-for="task in taskStore.favs" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
@@ -25,11 +30,14 @@
 </template>
 
 <script>
-import { ref } from "@vue/reactivity";
 import TaskDetails from "./components/TaskDetails.vue";
+import TaskForm from "./components/TaskForm.vue";
+
 import { useTaskStore } from "./stores/TaskStore";
+import { ref } from "vue";
+
 export default {
-  components: { TaskDetails },
+  components: { TaskDetails, TaskForm },
   setup() {
     const taskStore = useTaskStore();
 
--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -3,15 +3,28 @@
     <h3>
       {{ task.title }}
       <div class="icons">
-        <i class="material-icons">delete</i>
-        <i class="material-icons">favorite</i>
+        <i class="material-icons" @click="taskStore.deleteTask(task.id)">
+          delete
+        </i>
+        <i
+          class="material-icons"
+          :class="{ active: task.isFav }"
+          @click="taskStore.toggleFav(task.id)"
+        >
+          favorite
+        </i>
       </div>
     </h3>
   </div>
 </template>
 
 <script>
+import { useTaskStore } from "../stores/TaskStore";
 export default {
   props: ["task"],
+  setup() {
+    const taskStore = useTaskStore();
+    return { taskStore };
+  },
 };
 </script>

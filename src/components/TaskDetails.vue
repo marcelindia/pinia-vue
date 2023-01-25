c49c7e076b91cef6dd0cef026341cd3d03d61d25
Diana Marcelin <marcelindiana@yahoo.com>
2023-01-25 17:36:33 -0500

    added json server and loading class

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="task">
-    <h3>
-      {{ task.title }}
-      <div class="icons">
-        <i class="material-icons" @click="taskStore.deleteTask(task.id)">
-          delete
-        </i>
-        <i
-          class="material-icons"
-          :class="{ active: task.isFav }"
-          @click="taskStore.toggleFav(task.id)"
-        >
-          favorite
-        </i>
-      </div>
-    </h3>
+    <h3>{{ task.title }}</h3>
+    <div class="icons">
+      <i class="material-icons" @click="taskStore.deleteTask(task.id)">
+        delete
+      </i>
+      <i
+        class="material-icons"
+        :class="{ active: task.isFav }"
+        @click="taskStore.toggleFav(task.id)"
+      >
+        favorite
+      </i>
+    </div>
   </div>
 </template>
 
